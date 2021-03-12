7e003e0f50f6dc7f89deca256b24d13a9bd726f7
Iryna Zalata <iryna.zalata@gmail.com>
2021-03-12 20:14:00 +0200

    make it better

--- comments/comment.controllers.js
+++ comments/comment.controllers.js
@@ -4,7 +4,6 @@ async function getComments(req, res) {
   try {
     const comments = await Comment.find();
     res.json(comments);
-    res.send("<h1>HELLO</h1>");
   } catch (error) {
     res.status(400).send(error);
   }
