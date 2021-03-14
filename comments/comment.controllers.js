b68781d9febee008929a6e5e3bc5f8b19b868a28
Iryna Zalata <iryna.zalata@gmail.com>
2021-03-14 13:26:57 +0200

    Added final corrections

--- comments/comment.controllers.js
+++ comments/comment.controllers.js
@@ -7,7 +7,7 @@ dotenv.config();
 async function getComments(req, res) {
   try {
     const comments = await Comment.find();
-    res.json(comments) / send("You are not allowed to see this");
+    res.json(comments);
   } catch (error) {
     res.status(400).send(error);
   }
